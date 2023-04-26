b4c89369a4a9c05c3e413e666b888d2e537a5213
Danylo Holets <goletsdan@gmail.com>
2023-04-27 01:02:04 +0300

    Fixed window resolution and implemented "+" shape

--- ui/window.go
+++ ui/window.go
@@ -47,7 +47,9 @@ func (pw *Visualizer) run(s screen.Screen) {
 	}
 
 	w, err := s.NewWindow(&screen.NewWindowOptions{
-		Title: pw.Title,
+		Title:  pw.Title,
+		Width:  800,
+		Height: 800,
 	})
 	if err != nil {
 		log.Fatal("Failed to initialize the app window:", err)
@@ -134,6 +136,9 @@ func (pw *Visualizer) drawDefaultUI() {
 	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
 
 	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+
+	pw.w.Fill(image.Rect(200, 325, 600, 475), color.RGBA{R: 255, G: 255, B: 0, A: 1}, draw.Src)
+	pw.w.Fill(image.Rect(325, 200, 475, 600), color.RGBA{R: 255, G: 255, B: 0, A: 1}, draw.Src)
 
 	// Малювання білої рамки.
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
